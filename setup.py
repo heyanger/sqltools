4be6f70923f475bdcfd147d2c474656c0f5bdaaf
He Yang <erheyang@gmail.com>
2019-05-12 15:43:36 -0400

    ci stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from glob import glob
+
+from setuptools import find_packages
+
+setup(name='sqltools',
+      version='0.0.1',
+      description='sql tools for tree building/sequence generation',
+      author='He Yang, Er',
+      packages=find_packages('sqltools'),
+      package_dir={'': 'sqltools'},
+      py_modules=[splitext(basename(path))[0] for path in glob('sqltools/*.py')],
+     )
