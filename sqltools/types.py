949627da5419ae869a16738504b7e07f689114d0
er he yang <erheyang@gmail.com>
2019-05-25 13:40:33 -0400

    add compatibility for earlier python versions

--- sqltools/types.py
+++ sqltools/types.py
@@ -1,26 +1,26 @@
-from enum import Enum, auto
+from enum import Enum
 
 class State(Enum):
-    ROOT = auto()
-    SELECT = auto()
-    WHERE = auto()
-    FROM = auto()
-    GROUP_BY = auto()
-    ORDER_BY = auto()
-    LIMIT = auto()
-    HAVING = auto()
-    COL = auto()
-    OP = auto()
-    AGG = auto()
-    TERMINAL = auto()
-    TABLE = auto()
-    LOGIC = auto()
-    NONE = auto()
-    IUE = auto()
-    JOIN = auto()
-    DISTINCT = auto()
+    ROOT = 1
+    GROUP_BY = 2
+    ORDER_BY = 3
+    LIMIT = 4
+    HAVING = 5
+    COL = 6
+    OP = 7
+    AGG = 8
+    TERMINAL = 9
+    TABLE = 10
+    LOGIC = 11
+    NONE = 12
+    IUE = 13
+    JOIN = 14
+    DISTINCT = 15
+    SELECT = 16
+    WHERE = 17
+    FROM = 18
 
 class Seq(Enum):
-    copy = auto()
-    copyandchange = auto()
-    remove = auto()
+    copy = 1
+    copyandchange = 2
+    remove = 3
